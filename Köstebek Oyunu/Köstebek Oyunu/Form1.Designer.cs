c777b7153d2b32fd213fe81629b4975186f8c073
kubraacelik <kkbra.celik92@gmail.com>
2023-07-11 21:26:39 +0300

    new app developed

---
+++ Köstebek Oyunu/Köstebek Oyunu/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Köstebek_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblSure = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblSkor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(850, 432);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(868, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "KALAN SÜRE";
+            // 
+            // LblSure
+            // 
+            this.LblSure.AutoSize = true;
+            this.LblSure.BackColor = System.Drawing.Color.Transparent;
+            this.LblSure.Font = new System.Drawing.Font("Microsoft YaHei UI", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblSure.Location = new System.Drawing.Point(912, 63);
+            this.LblSure.Name = "LblSure";
+            this.LblSure.Size = new System.Drawing.Size(53, 39);
+            this.LblSure.TabIndex = 2;
+            this.LblSure.Text = "60";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(868, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SKOR :";
+            // 
+            // LblSkor
+            // 
+            this.LblSkor.AutoSize = true;
+            this.LblSkor.BackColor = System.Drawing.Color.Transparent;
+            this.LblSkor.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblSkor.Location = new System.Drawing.Point(961, 130);
+            this.LblSkor.Name = "LblSkor";
+            this.LblSkor.Size = new System.Drawing.Size(29, 31);
+            this.LblSkor.TabIndex = 4;
+            this.LblSkor.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 457);
+            this.Controls.Add(this.LblSkor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LblSure);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblSure;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblSkor;
+    }
+}
+
---
+++ Köstebek Oyunu/Köstebek Oyunu/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Köstebek_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int puan = 0;
+        Random rnd = new Random();
+        int sure = 60;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            //köstebekler için 66 adet buton oluşturdum
+            for (int i = 1; i < 67; i++) 
+            {
+                Button btn = new Button();
+                btn.BackColor = Color.White;
+                btn.ForeColor = Color.Black;
+                btn.Width = 50;
+                btn.Height = 50;
+                btn.Text = i.ToString();
+                flowLayoutPanel1.Controls.Add(btn); //butonu flowlayoutpanel içine ekledim 
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure--;
+            LblSure.Text = sure.ToString();
+
+            int rastgele = rnd.Next(1, 67); //sıçrama yapacağı butonu belirliyoruz
+
+            foreach (var a in flowLayoutPanel1.Controls) //flowLayoutPanel1.Controls içindeki her elemana a dedik
+            {
+                Button btn = a as Button;
+
+                if (btn.Text == rastgele.ToString()) //button'un text'i rastgele ile birbirini tutuyorsa o butonu kırmızı yap
+                {
+                    btn.BackColor = Color.Red;
+                    btn.Click += new EventHandler(btn_click);
+                }
+                else
+                {
+                    btn.BackColor = Color.White;
+                }
+            }
+
+            if (sure == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("OYUN SONLANMIŞTIR.");
+            }
+        }
+
+        void btn_click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button; //buton gibi gönder
+
+            if(btn.BackColor == Color.Red)
+            {
+                puan++;
+                LblSkor.Text = puan.ToString();
+            }
+            else
+            {
+                puan--;
+                LblSkor.Text = puan.ToString();
+            }
+
+            this.Text = puan.ToString();    
+        }
+    }
+}
